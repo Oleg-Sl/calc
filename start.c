a95d2aa3dedcfd67f72a8a2f571510a98ea6ba6b
Oleg <slepcov_oleg@mail.ru>
2022-09-15 22:55:52 +0700

    ''

--- start.c
+++ start.c
@@ -68,7 +68,7 @@ double calc(char* expression) {
                 token[0] = '~';
                 token[1] = '\0';
             }
-
+            printf("peek = %s \n", peek(stack_operator));
             while (stack_number && priorityOperator(peek(stack_operator)) >= priorityOperator(token)) {
                 calcByPolishNotationAlg(&stack_number, pop(&stack_operator));
             }
