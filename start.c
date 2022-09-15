507dd74924ca2674588e7477ea25fcecced09349
Oleg <slepcov_oleg@mail.ru>
2022-09-15 18:11:43 +0700

    ''

--- start.c
+++ start.c
@@ -6,12 +6,12 @@
 
 int main(void) {
     Node* stack = initStack();
-    push(stack, "111");
-    push(stack, "222");
-    push(stack, "333");
+    push(&stack, "111");
+    push(&stack, "222");
+    push(&stack, "333");
     printStack(stack);
-    printf("pop = %s\n", pop(stack));
-    printf("pop = %s\n", pop(stack));
+    printf("pop = %s\n", pop(&stack));
+    printf("pop = %s\n", pop(&stack));
     printStack(stack);
 }
 
