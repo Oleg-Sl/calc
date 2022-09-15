caf20ea3e793a493eff6e4339144a31d716df17c
Oleg <slepcov_oleg@mail.ru>
2022-09-15 23:32:03 +0700

    ''

--- s21_calc.h
+++
@@ -1,9 +0,0 @@
-#include <math.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-
-typedef struct Node {
-  struct Node* next;
-  char* str;
-} Node;
--- test.c
+++
@@ -1,7 +0,0 @@
-#include "s21_calc.h"
-
-int main() {
-  char *tmp = "123.45";
-  double d = strtod(tmp, NULL);
-  printf("%lf", d);
-}
